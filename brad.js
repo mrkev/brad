a1eeedf6d57ee989596a5dcfcc06ac14fd702f76
Ajay Gandhi <aag255@cornell.edu>
2015-08-25 01:37:30 -0400

    Fix errors for menu grabbing, add open/closed

--- brad.js
+++ brad.js
@@ -7,120 +7,201 @@
 // NPM modules
 var rp = require('request-promise');
 
+// API routes
 var menus_req = 'http://redapi-tious.rhcloud.com/dining/menu/ALL/ALL/LOCATIONS',
     time_url  = 'http://redapi-tious.rhcloud.com/dining/event/';
 
-// Validation regex
+// Meals and regex
 var meals  = ['Breakfast', 'Brunch', 'Lunch', 'Dinner'],
     m_patt = /(breakfast|brunch|lunch|dinner)/;
 
-// Responses for 
+// Responses for invalid input
 var inv_res =
   ['Sorry, I don\'t understand what you mean.',
    'wat',
    'do you even brad'];
 
-var hall_id = {
-  '104west':                           '104',
-  'amit_bhatia_libe_cafe':             'libe',
-  'atrium_cafe':                       'atrium',
-  'bear_necessities':                  'nasties',
-  'bears_den':                         'bears den',
-  'becker_house_dining_room':          'becker',
-  'big_red_barn':                      'big red barn',
-  'cafe_jennie':                       'jennie',
-  'carols_cafe':                       'carols',
-  'cascadeli':                         'cascadeli',
-  'cook_house_dining_room':            'cook',
-  'cornell_dairy_bar':                 'dairy bar',
-  'goldies':                           'goldies',
-  'green_dragon':                      'green dragon',
-  'ivy_room':                          'ivy room',
-  'jansens_dining_room_bethe_house':   'bethe',
-  'jansens_market':                    'jansens market',
-  'keeton_house_dining_room':          'keeton',
-  'marthas_cafe':                      'marthas',
-  'mattins_cafe':                      'mattins',
-  'north_star':                        'appel',
-  'okenshields':                       'okenshields',
-  'risley_dining':                     'risley',
-  'robert_purcell_marketplace_eatery': 'rpcc',
-  'rose_house_dining_room':            'rose',
-  'rustys':                            'rustys',
-  'synapsis_cafe':                     'synapsis',
-  'trillium':                          'trillium'
-};
-
 module.exports.ask = function (message) {
   message = message.toLowerCase();
+
+  var est_today, location;
 
   return rp(menus_req)
 
     .then(function (body) {
       var data = JSON.parse(body);
 
-      var location = Object.keys(data).reduce(function (p, c) {
-        var s = hall_id[c];
-        console.log(message.indexOf(s));
-        if (!p && message.indexOf(s) >= 0) return c;
-        else                               return p;
+      // Get location from message
+      location = Object.keys(data).reduce(function (loc, current) {
+        var s = shorten_name(current);
+        if (!loc && message.indexOf(s) >= 0) return current;
+        else                               return loc;
       }, false);
 
-      console.log(message);
-      console.log(location);
+      // No location, return a random invalid response
       if (!location)
         return inv_res[Math.floor(Math.random() * inv_res.length)];
 
-      var meal = m_patt.exec(message)[0];
+      var meal = m_patt.exec(message);
 
       // Specific meal menu
-      // if (meal) {
-        meal = meal.charAt(0).toUpperCase() + meal.slice(1);
+      if (meal) {
+        meal = meal[0].charAt(0).toUpperCase() + meal[0].slice(1);
 
         var meal_data = data[location][meal],
-            content   = '';
+            content   = [];
 
-        meal_data.forEach(function (o) {
-          content += o.name + (o.healthy ? ' 🍎' : '') + '\n';
-        });
+        // Open for that meal
+        if (meal_data) {
+          meal_data.forEach(function (m) {
+            content.push(m.name + (m.healthy ? ' 🍎' : ''));
+          });
 
-        return content;
+          return {
+            type: 'menu',
+            c:    content.join('\n')
+          };
+
+        // Closed for that meal
+        } else {
+          return {
+            type: 'menu',
+            c:    'Closed for ' + meal[0]
+          }
+        }
 
       // Is hall open
-      // } else {
-        // if (message.indexOf('open') < 0)
-        //   return inv_res[Math.floor(Math.random() * inv_res.length)];
+      } else {
+        if (message.indexOf('open') < 0)
+          return inv_res[Math.floor(Math.random() * inv_res.length)];
 
-        // // UTC tz offset for EST tz converted to ms
-        // var est_tz_offset = 14400000; // 240 * 60000
+        // UTC tz offset for EST tz converted to ms
+        var est_tz_offset = 14400000; // 240 * 60000
 
-        // var t         = new Date(),
-        //     utc_time  = t.getTime() + (t.getTimezoneOffset() * 60000),
-        //     est_today = new Date(utc_time - est_tz_offset),
-        //     est_tom   = new Date(),
+        var t         = new Date(),
+            utc_time  = t.getTime() + (t.getTimezoneOffset() * 60000),
+            est_tom   = new Date();
 
-        // est_tom.setDate(est_tom.getDate() + 1);
+        est_today = new Date(utc_time - est_tz_offset);
+        est_tom.setDate(est_tom.getDate() + 1);
 
-        // var before = est_today.toDateString().replace(/ /g, '%20'),
-        //     after  = est_tom.toDateString().replace(/ /g, '%20');
+        var before = est_today.toDateString().replace(/ /g, '%20'),
+            after  = est_tom.toDateString().replace(/ /g, '%20');
 
-        // var time_req = time_url + location + '/' + today + '-' + tomorrow;
+        var time_req = time_url + location + '/' + before + '-' + after;
 
-        // return rp(time_req);
+        return rp(time_req);
 
-      // }
+      }
 
     })
 
-    // .then(function (r) {
-    //   var j = JSON.parse(r);
+    .then(function (result) {
+      // Just return menu data
+      if (result.type && result.type === 'menu') {
+        return result.c;
 
-    //   if (j.type && j.type === 'menu') return j.content;
-    //   else                             return 'not implemented lolerz';
-    // })
+      // Open/closed query
+      } else {
+        var data = JSON.parse(result)[location];
+
+        // Open now
+        var atm = data.filter(function (event) {
+          var start = (new Date(event.start)).getTime();
+          var end   = (new Date(event.end)).getTime();
+          var now   = est_today.getTime();
+          return (start < now) && (now < end);
+        });
+
+        // Open sometime soon
+        var nxt = data.filter(function (event) {
+          var start = (new Date(event.start)).getTime();
+          var end   = (new Date(event.end)).getTime();
+          var now   = (new Date()).getTime();
+          return (now < start) && (now < end);
+        })
+        .sort(function (a, b) {
+          a = (new Date(a.start)).getTime();
+          b = (new Date(b.start)).getTime();
+          return a - b;
+        });
+
+        // Open now
+        if (atm.length > 0) {
+          return atm[0].summary;
+
+        } else {
+
+          if (nxt.length > 0) {
+            var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                          'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+            var opens = new Date(nxt[0].start);
+            console.log(opens);
+
+            var ret = days[opens.getDay()] + ', ' + months[opens.getMonth()];
+                ret += ' ' + opens.getDate();
+
+            var hrs = opens.getHours();
+            var ap  = 'am';
+
+            if (hrs >= 12) {
+              hrs -= 12;
+              ap = 'pm';
+            }
+            hrs = (hrs == 0) ? 12 : hrs;
+
+            var m = opens.getMinutes();
+
+            return 'Closed right now. Opens on ' + ret + ' at ' + hrs + ':' +
+              ((m < 10) ? '0' + m : m) + ap + '.';
+
+          // Closed and not open soon
+          } else {
+            return 'Closed right now.';
+          }
+        }
+      }
+    })
 
     .catch(function (e) {
       console.trace(e);
-      return 'Couldn\'t get menus :(';
-    })
+      return 'Couldn\'t get info :(';
+    });
 };
+
+var shorten_name = function (lg) {
+  var hall_id = {
+    '104west':                           '104',
+    'amit_bhatia_libe_cafe':             'libe',
+    'atrium_cafe':                       'atrium',
+    'bear_necessities':                  'nasties',
+    'bears_den':                         'bears den',
+    'becker_house_dining_room':          'becker',
+    'big_red_barn':                      'big red barn',
+    'cafe_jennie':                       'jennie',
+    'carols_cafe':                       'carols',
+    'cascadeli':                         'cascadeli',
+    'cook_house_dining_room':            'cook',
+    'cornell_dairy_bar':                 'dairy bar',
+    'goldies':                           'goldies',
+    'green_dragon':                      'green dragon',
+    'ivy_room':                          'ivy room',
+    'jansens_dining_room_bethe_house':   'bethe',
+    'jansens_market':                    'jansens market',
+    'keeton_house_dining_room':          'keeton',
+    'marthas_cafe':                      'marthas',
+    'mattins_cafe':                      'mattins',
+    'north_star':                        'appel',
+    'okenshields':                       'okenshields',
+    'risley_dining':                     'risley',
+    'robert_purcell_marketplace_eatery': 'rpcc',
+    'rose_house_dining_room':            'rose',
+    'rustys':                            'rustys',
+    'synapsis_cafe':                     'synapsis',
+    'trillium':                          'trillium'
+  }
+
+  return hall_id[lg];
+}
--- index.js
+++ index.js
@@ -1,47 +1,58 @@
+'use strict';
+
+// NPM modules
 var voicejs = require('voice.js');
+
+// Local modules
 var brad = require('./brad')
 
-
 var client = new voicejs.Client({
-  email: process.argv[2] || 'brad@voice.js',
+  email:    process.argv[2] || 'brad@voice.js',
   password: process.argv[3] || 'qwertqwert',
-  tokens: require('./tokens.json')
+  tokens:   require('./tokens.json')
 });
 
 /**
- * Gets all unread messages inbox.
- * @return {Promise} to an array of question objects {phone_number, id, timestamp, last_message}
+ * Gets all unread messages from inbox
+ *
+ * @return {Promise} to an array of question objects
+ *   {phone_number, id, timestamp, last_message}
  */
 var get_questions = function () {
-  return new Promise (function (res, rej) {
+  return new Promise (function (resolve) {
     // Get all conversations, not including spam. 
     // WARNING: MAY BE SLOW FOR VERY LARGE DATASETS
-    client.get('unread', {limit: Infinity}, function(error, response, data){
-      if (error) {return console.trace(error);}
-      if (!data || !data.conversations_response || !data.conversations_response.conversationgroup){ return console.log('No conversations.'); }
+    client.get('unread', { limit: Infinity }, function (err, response, data) {
+      if (err) return console.trace(err);
 
-      // Display the conversations in descending order
-      data.conversations_response.conversationgroup.reverse().forEach(function(convo, index){
-        console.log('%s %s. %s %s',
+      if (!data ||
+          !data.conversations_response ||
+          !data.conversations_response.conversationgroup)
+        return console.log('No conversations.');
+
+      var convos = data.conversations_response.conversationgroup;
+
+      // Print each unread
+      convos.forEach(function (convo, index) {
+        console.log('%s from %s: %s',
           new Date(convo.conversation.conversation_time).toISOString().replace(/[ZT]/g,' ').substr(0,16),
           convo.call[0].phone_number,
-          convo.conversation.id,
-          convo.conversation.message_text[0]
+          convo.conversation.message_text.slice(-1)[0]
         );
       });
       
-      console.log(data.conversations_response.conversationgroup.length +  ' conversations retrieved');
+      console.log(convos.length +  ' conversations retrieved');
       
-      var questions = data.conversations_response.conversationgroup.map(function (convo) {
+      var questions = convos.map(function (convo) {
         return {
-          phone_number : convo.call[0].phone_number,
-          id : convo.conversation.id,
-          timestamp : new Date(convo.conversation.conversation_time),
-          last_message : convo.conversation.message_text[0]
+          phone_number: convo.call[0].phone_number,
+          id:           convo.conversation.id,
+          timestamp:    new Date(convo.conversation.conversation_time),
+          last_message: convo.conversation.message_text.slice(-1)[0]
         }
       });
 
-      res(questions);
+      resolve(questions);
     });
   });
 }
@@ -52,56 +63,59 @@ var get_questions = function () {
  * @return {Promise}         to {phone_number, id, message}
  */
 var answer = function (question) {
-  // console.log(question);
-
-  return brad.ask(question.last_message).then(function (message) {
-    var ans = {
-      phone_number : question.phone_number,
-      id : question.id,
-      message : message
-    };
-    return ans;
-  });
+  return brad
+    .ask(question.last_message)
+    .then(function (message) {
+      return {
+        phone_number: question.phone_number,
+        id:           question.id,
+        message:      message
+      };
+    });
 }
 
 /**
  * Sends answer objects, and marks the conversations as read.
+ *
  * @param  {Object} ans {phone_number, id, message}
+ *
  * @return {Promise}    to nothing lol.
  */
 var send = function (ans) {
-  return new Promise(function (res, rej) {
+  return new Promise(function (resolve) {
     // console.log('Would send to', ans.phone_number, ':', ans.message);
     
     var response = {
-      to: ans.phone_number,
+      to:   ans.phone_number,
       text: ans.message
     }
 
     var done = {
-      id : ans.id,
-      read : true
+      id:   ans.id,
+      read: true
     }
 
     console.log(response);
     client.sms(response, function () {
-      console.log('b');
-      client.set('mark', done, res);
+      client.set('mark', done, resolve);
     });
   });
 }
 
 
 var main = function () {
-  console.log('yay');
+  // Get incoming unread messages
   get_questions().then(function (questions) {
+    // Handle messages
     return Promise.all(questions.map(answer))
   })
   .then(function (answers) {
+    // Send messages
     return Promise.all(answers.map(send))
   })
-  .catch(console.trace)
+  .catch(console.trace);
 
+  // Do this every 5s
   setTimeout(main.bind(this), 5000);
 }
 
